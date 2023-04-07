76b6801e7034c655509eaf17c616ff9f27bee33f
SkiroNako <2681678536@qq.com>
2023-04-07 17:04:00 +0800

    使用Json格式返回结果

--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
@@ -1,12 +1,16 @@
 package com.atguigu.yygh.hosp.controller;
 
 
+import com.atguigu.yygh.common.utils.Result;
 import com.atguigu.yygh.hosp.service.HospitalSetService;
+import com.atguigu.yygh.model.hosp.HospitalSet;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * <p>
@@ -26,14 +30,19 @@ public class HospitalSetController {
 
     @ApiOperation("查询所有")
     @GetMapping("/findAll")
-    public Object findAll(){
-        return hospitalSetService.list();
+    public Result findAll(){
+        List<HospitalSet> hospitalSetList = hospitalSetService.list();
+        return Result.ok().data("hospitalSetList",hospitalSetList);
     }
 
     @ApiOperation("根据id删除")
     @DeleteMapping("/{id}")
-    public Object deleteById(@ApiParam("医院id") @PathVariable Long id){
-        return hospitalSetService.removeById(id);
+    public Result deleteById(@ApiParam("医院id") @PathVariable Long id){
+
+        if(hospitalSetService.removeById(id))
+            return Result.ok();
+
+        return Result.error();
     }
 
 
