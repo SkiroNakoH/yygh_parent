22ae6d963f9afb94af3909a8942157fa3f8d9940
SkiroNako <2681678536@qq.com>
2023-04-07 20:15:07 +0800

    分页和条件查询-解决mybatis-plus依赖冲突

--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
@@ -4,10 +4,16 @@ package com.atguigu.yygh.hosp.controller;
 import com.atguigu.yygh.common.utils.Result;
 import com.atguigu.yygh.hosp.service.HospitalSetService;
 import com.atguigu.yygh.model.hosp.HospitalSet;
+import com.atguigu.yygh.vo.hosp.HospitalSetQueryVo;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+
+import com.baomidou.mybatisplus.core.metadata.OrderItem;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -45,6 +51,28 @@ public class HospitalSetController {
         return Result.error();
     }
 
+
+    @ApiOperation("分页条件查询")
+    @GetMapping("/pageList/{page}/{size}")
+    public Result pageList(@ApiParam("当前页数") @PathVariable Integer page,
+                           @ApiParam("每页显示条数") @PathVariable Integer size,
+                           @ApiParam("查询条件") HospitalSetQueryVo hospitalSetQueryVo){
+        Page<HospitalSet> hospitalSetPage = new Page<>(page, size);
+
+        //查询条件--根据医院名称模糊查询、根据医院code查询
+        QueryWrapper<HospitalSet> queryWrapper = new QueryWrapper<>();
+        queryWrapper.like(!StringUtils.isEmpty(hospitalSetQueryVo.getHosname()), "hosname",hospitalSetQueryVo.getHosname());
+        queryWrapper.eq(!StringUtils.isEmpty(hospitalSetQueryVo.getHoscode()),"hoscode",hospitalSetQueryVo.getHoscode());
+
+
+
+        hospitalSetService.page(hospitalSetPage,queryWrapper);
+
+        List<HospitalSet> hospitalSetList = hospitalSetPage.getRecords();
+        long total = hospitalSetPage.getTotal();
+
+        return Result.ok().data("hospitalSetList",hospitalSetList).data("total",total);
+    }
 
 }
 
