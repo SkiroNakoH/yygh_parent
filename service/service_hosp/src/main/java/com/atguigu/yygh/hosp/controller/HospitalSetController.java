9afc4bfdcb9fb234c3a6b4cffac8f80b5b0aea1a
SkiroNako <2681678536@qq.com>
2023-04-07 20:33:48 +0800

    新增和修改

--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
@@ -1,6 +1,7 @@
 package com.atguigu.yygh.hosp.controller;
 
 
+import com.atguigu.yygh.common.utils.MD5;
 import com.atguigu.yygh.common.utils.Result;
 import com.atguigu.yygh.hosp.service.HospitalSetService;
 import com.atguigu.yygh.model.hosp.HospitalSet;
@@ -17,6 +18,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
  * <p>
@@ -36,16 +38,16 @@ public class HospitalSetController {
 
     @ApiOperation("查询所有")
     @GetMapping("/findAll")
-    public Result findAll(){
+    public Result findAll() {
         List<HospitalSet> hospitalSetList = hospitalSetService.list();
-        return Result.ok().data("hospitalSetList",hospitalSetList);
+        return Result.ok().data("hospitalSetList", hospitalSetList);
     }
 
     @ApiOperation("根据id删除")
     @DeleteMapping("/{id}")
-    public Result deleteById(@ApiParam("医院id") @PathVariable Long id){
+    public Result deleteById(@ApiParam("医院id") @PathVariable Long id) {
 
-        if(hospitalSetService.removeById(id))
+        if (hospitalSetService.removeById(id))
             return Result.ok();
 
         return Result.error();
@@ -56,23 +58,51 @@ public class HospitalSetController {
     @GetMapping("/pageList/{page}/{size}")
     public Result pageList(@ApiParam("当前页数") @PathVariable Integer page,
                            @ApiParam("每页显示条数") @PathVariable Integer size,
-                           @ApiParam("查询条件") HospitalSetQueryVo hospitalSetQueryVo){
+                           @ApiParam("查询条件") HospitalSetQueryVo hospitalSetQueryVo) {
         Page<HospitalSet> hospitalSetPage = new Page<>(page, size);
 
         //查询条件--根据医院名称模糊查询、根据医院code查询
         QueryWrapper<HospitalSet> queryWrapper = new QueryWrapper<>();
-        queryWrapper.like(!StringUtils.isEmpty(hospitalSetQueryVo.getHosname()), "hosname",hospitalSetQueryVo.getHosname());
-        queryWrapper.eq(!StringUtils.isEmpty(hospitalSetQueryVo.getHoscode()),"hoscode",hospitalSetQueryVo.getHoscode());
+        queryWrapper.like(!StringUtils.isEmpty(hospitalSetQueryVo.getHosname()), "hosname", hospitalSetQueryVo.getHosname());
+        queryWrapper.eq(!StringUtils.isEmpty(hospitalSetQueryVo.getHoscode()), "hoscode", hospitalSetQueryVo.getHoscode());
 
 
-
-        hospitalSetService.page(hospitalSetPage,queryWrapper);
+        hospitalSetService.page(hospitalSetPage, queryWrapper);
 
         List<HospitalSet> hospitalSetList = hospitalSetPage.getRecords();
         long total = hospitalSetPage.getTotal();
 
-        return Result.ok().data("hospitalSetList",hospitalSetList).data("total",total);
+        return Result.ok().data("hospitalSetList", hospitalSetList).data("total", total);
     }
 
+
+    @ApiOperation("新增医院")
+    @PostMapping("/insert")
+    public Result insert(@ApiParam("医院信息") @RequestBody HospitalSet hospitalSet) {
+        hospitalSet.setSignKey(MD5.encrypt(UUID.randomUUID().toString()));
+
+
+        boolean save = hospitalSetService.save(hospitalSet);
+        if (save)
+            return Result.ok();
+        return Result.error();
+    }
+
+    @ApiOperation("根据id查询")
+    @GetMapping("/{id}")
+    public Result getById(@ApiParam("医院id") @PathVariable Long id){
+        HospitalSet hospitalSet = hospitalSetService.getById(id);
+        return Result.ok().data("hospitalSet",hospitalSet);
+    }
+
+    @ApiOperation("根据id修改")
+    @PutMapping("/updateById")
+    public Result updateById(@RequestBody HospitalSet hospitalSet){
+
+        boolean update = hospitalSetService.updateById(hospitalSet);
+        if (update)
+            return Result.ok();
+        return Result.error();
+    }
 }
 
