47e45f6e199841cab739d485043baa1313683e24
SkiroNako <2681678536@qq.com>
2023-04-17 18:32:34 +0800

    新增网关模块

---
+++ service_gateway/src/main/java/com/atguigu/yygh/gateway/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.atguigu.yygh.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");   //允许所有请求方法   get,post,put,delete...
+        config.addAllowedOrigin("*");   //解决跨域请求问题
+        config.addAllowedHeader("*");   //允许所有请求头，请求头中可以传递任意参数
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
